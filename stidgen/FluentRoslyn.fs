b258061bf92203aaadefdc08034d40e44da33b94
Julien Roncaglia <julien@roncaglia.fr>
2015-04-26 16:43:07 +0200

    Add more fluent roslyn utilities
    Also document most of the existing ones

--- stidgen/FluentRoslyn.fs
+++ stidgen/FluentRoslyn.fs
@@ -11,31 +11,32 @@ module Operators =
     /// Unary operator to convert C# Task<'t> to F# Async<'t>
     let (!!) t = Async.AwaitTask t
 
-type CompilationUnitSyntax with
-    member this.AddUsings([<System.ParamArrayAttribute>] items : string array) =
-        let directives =
-            items
-            |> Array.map (fun name -> SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName(name)))
-
-        this.AddUsings(directives)
-
 type TypeSyntax with
     static member Object = SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.ObjectKeyword))
     static member String = SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.StringKeyword))
     static member Int = SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.IntKeyword))
     static member Bool = SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.BoolKeyword))
+    static member FromType (t:System.Type) = SyntaxFactory.ParseTypeName(t.FullName)
 
 module Literal = 
     let Null = SyntaxFactory.LiteralExpression(SyntaxKind.NullLiteralExpression)
     let True = SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression)
     let False = SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)
     let Bool (b:bool) = if b then True else False
-
+   
     let String (s:string) =
         SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(s))
+    let EmptyString = String ""
 
     let Int (i:int) =
         SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(i))
+    let Zero = Int 0
+
+let addUsings (usings : string array) (compilationUnit : CompilationUnitSyntax) =
+    let directives = usings |> Array.map (fun name ->
+        SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName(name)))
+
+    compilationUnit.AddUsings(directives)
 
 let inline addModifiers syntaxKinds (input:^T) =
     let tokens = syntaxKinds |> Array.map (fun k -> SyntaxFactory.Token(k))
@@ -55,7 +56,7 @@ let inline addArgument expression (input:^T) =
 
 let inline addBodyStatement statement (input:^T) =
     (^T : (member AddBodyStatements : StatementSyntax array -> ^T) (input, [|statement|]))
-    
+   
 let inline addMember member' (input:^T) =
     (^T : (member AddMembers : MemberDeclarationSyntax array -> ^T) (input, [|member'|]))
 
@@ -66,6 +67,7 @@ let inline withBody (statements: StatementSyntax array) (input:^T) =
     let block = SyntaxFactory.Block(SyntaxFactory.List<StatementSyntax>(statements))
     (^T : (member WithBody : BlockSyntax -> ^T) (input, block))
 
+/// this.memberName = value;
 let setThisMember (memberName:string) value =
     SyntaxFactory.ExpressionStatement(
         SyntaxFactory.AssignmentExpression(
@@ -81,6 +83,7 @@ let setThisMember (memberName:string) value =
 
 let identifier (identifierName : string) = SyntaxFactory.IdentifierName(identifierName)
 
+/// onExpr.name
 let memberAccess (name : string) (onExpr : ExpressionSyntax) =
     SyntaxFactory.MemberAccessExpression(
         SyntaxKind.SimpleMemberAccessExpression,
@@ -101,15 +104,15 @@ let dottedMemberAccess' identifiers =
     | [] -> failwith "No identifiers provided"
     | first::rest -> dottedMemberAccess rest (identifier first)
 
-let staticMemberAccess (``member``:string) (``type`` : TypeSyntax) =
-    ``type``
-
+/// id.member
 let simpleMemberAccess (id:string) (``member``:string) =
     (identifier id) |> memberAccess  ``member``
 
+/// this.member
 let thisMemberAccess (``member``:string) =
     SyntaxFactory.ThisExpression() |> memberAccess ``member``
 
+/// new createdType(argumentExpressions)
 let objectCreation createdType argumentExpressions =
     let args = argumentExpressions |> Array.map (fun a -> SyntaxFactory.Argument(a))
     let argList = SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList<ArgumentSyntax>(args))
@@ -124,8 +127,41 @@ let invocation (expression : ExpressionSyntax) (argumentExpressions : Expression
     SyntaxFactory.InvocationExpression(expression)
         .WithArgumentList(argList)
 
+/// return expression;
 let ret expression = SyntaxFactory.ReturnStatement(expression)
+
+/// (expression)
 let parenthesis expression = SyntaxFactory.ParenthesizedExpression(expression)
+
+/// ((toType) expression)
 let cast toType expression = parenthesis (SyntaxFactory.CastExpression(toType, expression))
+
+/// (expression is checkedType)
 let is checkedType expression = parenthesis(SyntaxFactory.BinaryExpression(SyntaxKind.IsExpression, expression, checkedType))
+
+/// (left == right)
+let equals left right = parenthesis (SyntaxFactory.BinaryExpression(SyntaxKind.EqualsExpression, left, right))
+
+/// !(expression)
 let not' expression = SyntaxFactory.PrefixUnaryExpression(SyntaxKind.LogicalNotExpression, parenthesis expression)
+
+/// if (condition) then then'
+let if' condition then' = SyntaxFactory.IfStatement(condition, then')
+
+/// if (condition) then then' else else'
+let ifelse condition then' else' = SyntaxFactory.IfStatement(condition, then', SyntaxFactory.ElseClause(else'))
+
+/// {}
+let emptyBlock = SyntaxFactory.Block()
+
+/// { statements }
+let block (statements : StatementSyntax array) = SyntaxFactory.Block(statements)
+
+/// throw expression;
+let throw expression = SyntaxFactory.ThrowStatement(expression)
+
+/// typeof('t)
+let typenameof<'t> = TypeSyntax.FromType(typedefof<'t>)
+
+/// An empty file ("compilation unit")
+let emptyFile = SyntaxFactory.CompilationUnit()
