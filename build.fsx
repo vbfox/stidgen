30f962f855b3473c7fe03662206def81f2082fb7
Julien Roncaglia <julien@roncaglia.fr>
2015-05-02 02:39:15 +0200

    Make the build file correctly run tests

--- build.fsx
+++ build.fsx
@@ -5,33 +5,34 @@ open System.IO
 
 let nunitPath = @"packages\NUnit.Runners\tools"
 let binDir = Path.Combine(__SOURCE_DIRECTORY__, "bin")
-let buildDir = Path.Combine(binDir, "build")
 let testsDir = Path.Combine(binDir, "tests")
 
 let solutionFile  = "stidgen.sln"
+let testAssemblies = "tests/**/bin/Release/*.Tests.dll"
 
 // Copies binaries from default VS location to exepcted bin folder
 // But keeps a subdirectory structure for each project in the 
 // src folder to support multiple project outputs
 Target "CopyBinaries" (fun _ ->
+    CreateDir binDir
     !! "src/**/*.??proj"
     |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", binDir @@ (System.IO.Path.GetFileNameWithoutExtension f)))
     |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
 )
 
 Target "Build" (fun _ ->
-    CreateDir buildDir
     !! solutionFile
     |> MSBuildRelease "" "Rebuild"
     |> ignore
 )
 
 Target "RunTests" (fun _ ->
-    !! (testsDir + "/UnitTests.dll")
+    !! testAssemblies
       |> NUnit (fun p ->
           {p with
              ToolPath = nunitPath
-             DisableShadowCopy = true;
+             DisableShadowCopy = true
+             TimeOut = TimeSpan.FromMinutes 20.
              OutputFile = testsDir + "TestResults.xml" })
 )
 
