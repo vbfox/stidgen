67817a9bc1ca31f9ea64e76eb3ad51339d670d0e
Julien Roncaglia <julien@roncaglia.fr>
2015-06-02 15:08:00 +0200

    Use invert pipe instead of parenthesis in .fsx
    It's F#, not lisp...

--- build.fsx
+++ build.fsx
@@ -13,20 +13,18 @@ let testAssemblies = "tests/**/bin/Release/*.Tests.dll"
 // Copies binaries from default VS location to exepcted bin folder
 // But keeps a subdirectory structure for each project in the 
 // src folder to support multiple project outputs
-Target "CopyBinaries" (fun _ ->
+Target "CopyBinaries" <|fun _ ->
     CreateDir binDir
     !! "src/**/*.??proj"
     |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", binDir @@ (System.IO.Path.GetFileNameWithoutExtension f)))
     |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
-)
 
-Target "Build" (fun _ ->
+Target "Build" <|fun _ ->
     !! solutionFile
     |> MSBuildRelease "" "Rebuild"
     |> ignore
-)
 
-Target "RunTests" (fun _ ->
+Target "RunTests" <| fun _ ->
     !! testAssemblies
       |> NUnit (fun p ->
           {p with
@@ -34,19 +32,12 @@ Target "RunTests" (fun _ ->
              DisableShadowCopy = true
              TimeOut = TimeSpan.FromMinutes 20.
              OutputFile = testsDir + "TestResults.xml" })
-)
 
-Target "Clean" (fun _ ->
-    CleanDir binDir
-)
+Target "Clean" <| fun _ -> CleanDir binDir
 
-Target "Default" (fun _ ->
-    trace "Default target executed"
-)
+Target "Default" <|fun _ -> trace "Default target executed"
 
-Target "Paket" (fun _ ->
-    trace "Paket should have been executed"
-)
+Target "Paket" <| fun _ -> trace "Paket should have been executed"
 
 "Clean"
   ==> "Build"
