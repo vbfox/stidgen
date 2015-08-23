445f50dfcdc58e8dfaa91cd38b94f84bca62faba
Julien Roncaglia <julien@roncaglia.fr>
2015-08-23 23:03:02 +0200

    Add Glob support
    The command line now both support multiple files and globs
    
    Fixes #32

--- src/BlackFox.Stidgen/Program.fs
+++ src/BlackFox.Stidgen/Program.fs
@@ -1,19 +1,45 @@
 ﻿module BlackFox.Stidgen.Program
 
-let printUsage () =
+type runResult = (string  *ConfigurationParser.ParseError list) list
+
+let run (fileGlobs : string seq) : runResult =
+    let baseDir = System.Environment.CurrentDirectory
+    
+    fileGlobs
+        |> Seq.collect (fun glob -> Fake.Globbing.search baseDir glob)
+        |> Seq.map (fun f -> f, FileGeneration.generateToFiles f)
+        |> List.ofSeq
+
+let isSuccess (result: runResult) = 
+    let errorCount = result |> Seq.collect snd |> Seq.length
+    errorCount = 0
+
+let private printUsage () =
     printfn "Usage:"
     printfn "    stidgen fileName"
 
+let private printResult (result: runResult) =
+    for file, errors in result do
+        match errors with
+        | [] -> ()
+        | errors ->
+            eprintfn "In %s:" file
+            for error in errors do
+                eprintfn "\t%O" error
+                eprintfn "\t\tContent: %s" error.Line.Text
+            eprintfn ""
+
+let private getExitCode (result: runResult) = 
+    match isSuccess result with
+    | true -> 0
+    | false -> -1
+
 [<EntryPoint>]
 let main argv = 
-    if argv.Length = 1 then
-        match FileGeneration.generateToFiles argv.[0] with
-        | [] -> 0
-        | errors ->
-            for error in errors do
-                printfn "%O" error
-                printfn "\tContent: %s" error.Line.Text
-            -1
+    if argv.Length > 0 then
+        let result = run argv
+        printResult result
+        getExitCode result
     else
         printUsage ()
         0
