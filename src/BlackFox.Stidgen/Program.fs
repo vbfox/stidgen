9fb9066ace35f859036255d47ec7738e42c8f8b6
Julien Roncaglia <julien@roncaglia.fr>
2016-02-27 15:26:35 +0100

    Add color output & message when nothing is selected

--- src/BlackFox.Stidgen/Program.fs
+++ src/BlackFox.Stidgen/Program.fs
@@ -1,36 +1,55 @@
 ﻿module BlackFox.Stidgen.Program
 
+open BlackFox
+
 type RunResults = FileGeneration.GenerationResult list
 
-let run (fileGlobs : string seq) : RunResults =
+let run fileGlobs =
     let baseDir = System.Environment.CurrentDirectory
     
-    fileGlobs
-        |> Seq.collect (fun glob -> Fake.Globbing.search baseDir glob)
-        |> Seq.map FileGeneration.generateToFiles
-        |> List.ofSeq
+    [
+        for glob in fileGlobs do
+            let outputs = [
+                for file in Fake.Globbing.search baseDir glob do
+                    yield FileGeneration.generateToFiles file
+            ]
+            yield glob, outputs
+    ]
 
 let private printUsage () =
-    printfn "Usage:"
-    printfn "    stidgen fileGlob [fileGlob ...]"
+    sprintf "^[yellow]Usage^[reset]:" |> coloredWriteLine
+    sprintf "    ^[white]stidgen^[reset] fileGlob ^[darkgray][^[reset]fileGlob ...^[darkgray]]^[reset]" |> coloredWriteLine
 
-let private printResult (results: RunResults) =
-    for result in results do
-        if result.HasErrors() then
-            eprintfn "Errors in %s:" result.Configuration.Path.Value
-            for error in result.Configuration.Errors do
-                eprintfn "\t%O" error
-                eprintfn "\t\tContent: %s" error.Line.Text
-            eprintfn ""
-        else
-            for file in result.Files do
-                printfn "Generated in %s:" file.FileName
-                for generated in file.IdTypes do
-                    printfn "\t* %s" generated.Name
+let private printSuccessResult (result:FileGeneration.GenerationResult) =
+    for file in result.Files do
+        sprintf "Generated in ^[yellow]%s^[reset]:" file.FileName |> coloredWriteLine
+        for generated in file.IdTypes do
+            sprintf "  ^[darkgray]*^[white] %s" generated.Name |> coloredWriteLine
+    printfn ""
+
+let private printErrorResult (result:FileGeneration.GenerationResult) = 
+    sprintf "^[red]Errors in ^[yellow]%s^[red]:" result.Configuration.Path.Value |> coloredWriteLine
+    for error in result.Configuration.Errors do
+        sprintf "^[red]\t%O" error |> coloredWriteLine
+        sprintf "^[red]\t\tContent: %s" error.Line.Text |> coloredWriteLine
+    printfn ""
+
+let private printResult (results: (string*RunResults) list) =
+    for glob, results in results do
+        if results.IsEmpty then
+            sprintf "^[red]No files found for '^[yellow]%s^[red]'" glob |> coloredWriteLine
             printfn ""
+        else
+            for result in results do
+                if result.HasErrors() then
+                    printErrorResult result
+                else
+                    printSuccessResult result
 
-let private getExitCode (result: RunResults) = 
-    if result |> Seq.exists(fun r -> r.HasErrors()) then
+let private getExitCode (result: (string*RunResults) list) = 
+    let hasErrors = result |> List.collect snd |> List.exists(fun r -> r.HasErrors())
+    let noFiles = result |> List.collect snd |> List.isEmpty
+    if hasErrors || noFiles then
         -1
     else
         0
