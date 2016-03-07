887fd71f174606170cc27fa68b0c1ad0a6382df0
Julien Roncaglia <julien@roncaglia.fr>
2016-03-07 22:18:36 +0100

    Use task instead of target in build script

---
+++ build/TaskDefinitionHelper.fsx
@@ -0,0 +1,54 @@
+#r "../packages/FAKE/tools/FakeLib.dll"
+
+namespace BlackFox
+
+/// Allow to define FAKE tasks with a syntax similar to Gulp tasks
+module TaskDefinitionHelper =
+    open Fake
+    open System.Text.RegularExpressions
+
+    type Dependency =
+        | Direct of dependsOn : string
+        | Soft of dependsOn : string
+        | Conditional of dependsOn : string * condition : bool
+
+    let private (|RegExp|_|) pattern input =
+        let m = Regex.Match(input, pattern, RegexOptions.Compiled)
+        if m.Success && m.Groups.Count > 0 then
+            Some (m.Groups.[1].Value)
+        else
+            None
+
+    let private parseDependency str =
+        match str with
+        | RegExp @"^\?(.*)$" dep -> Soft dep
+        | dep -> Direct dep
+
+    let mutable private tasks : (string * (Dependency list)) list = []
+    
+    /// Define a task with it's dependencies
+    let taskEx name dependencies body =
+        Target name body
+        tasks <- (name, dependencies |> List.ofSeq) :: tasks
+
+    /// Define a task with it's dependencies
+    let task name dependencies body =
+        let dependencies = dependencies |> Seq.map parseDependency
+        taskEx name dependencies body
+
+    /// Send all the defined inter task dependencies to FAKE
+    let applyTasksDependencies () =
+         for (targetName, dependencies) in tasks do
+            for dependency in dependencies do
+                match dependency with
+                | Direct dep -> dep ==> targetName |> ignore
+                | Soft dep -> dep ?=> targetName |> ignore
+                | Conditional (dep, cond) -> dep =?> (targetName, cond) |> ignore
+
+         tasks <- []
+
+    /// Run the task specified on the command line if there was one or the
+    /// default one otherwise.
+    let runTaskOrDefault taskName =
+        applyTasksDependencies ()
+        RunTargetOrDefault taskName
