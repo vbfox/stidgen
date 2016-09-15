396d1a8c92302ce9f8c693a6ed3e9333336df636
Julien Roncaglia <julien@roncaglia.fr>
2016-09-15 15:40:25 +0200

    Use a fixed paket version (3.19.4)

--- paket.cmd
+++ paket.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-.paket\paket.bootstrapper.exe -s
+.paket\paket.bootstrapper.exe 3.19.4 -s
 if errorlevel 1 (
   exit /b %errorlevel%
 )
--- paket.sh
+++ paket.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mono .paket/paket.bootstrapper.exe -s
+mono .paket/paket.bootstrapper.exe 3.19.4 -s
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
   exit $exit_code
