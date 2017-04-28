d4f18729ee4633e17e09f107ffc489c9ddf3879b
Julien Roncaglia <julien@roncaglia.fr>
2017-04-28 11:58:16 +0200

    Move more tests to expecto

--- tests/BlackFox.Stidgen.Tests/ParseTests.fs
+++ tests/BlackFox.Stidgen.Tests/ParseTests.fs
@@ -1,126 +1,143 @@
 ﻿module BlackFox.Stidgen.ParseTests
 
 open BlackFox.Stidgen.CsharpCodeTesting
-open BlackFox.Stidgen.CsharpGeneration
 open BlackFox.Stidgen.Description
-open NUnit.Framework
 open System
+open Expecto.Tests
+open System.Globalization
+open System.Threading
+open Expecto
 
-[<AbstractClass>]
-type ParsingTests() =
+let withCulture (culture: string) f () =    
+    let previous = Thread.CurrentThread.CurrentCulture
+    try
+        Thread.CurrentThread.CurrentCulture <- CultureInfo.GetCultureInfo(culture)
+        f ()
+    finally
+        Thread.CurrentThread.CurrentCulture <- previous
+
+let testWithCulture name culture f =
+    testCase name (withCulture culture f)
+
+type IParseTests =
     abstract member testCorrectTryParse<'t> : string -> string -> string -> unit
     abstract member testInvalidTryParse<'t> : string -> string -> unit
 
-    [<Test>] [<SetCulture("en-US")>]
-    member x.``good Int32`` () = x.testCorrectTryParse<int> "42" "" "IsEqualTo(42)"
+let standardTests (x: IParseTests) = [
+    testWithCulture "good Int32" "en-US" <| fun () ->
+        x.testCorrectTryParse<int> "42" "" "IsEqualTo(42)"
 
-    [<Test>]
-    member x.``good Int32 hex`` () =
+    test "good Int32 hex" {
         x.testCorrectTryParse<int> "2A" ", NumberStyles.HexNumber, CultureInfo.InvariantCulture" "IsEqualTo(42)"
+    }
 
-    [<Test>] [<SetCulture("en-US")>]
-    member x.``bad Int32`` () = x.testInvalidTryParse<int> "foo" ""
+    testWithCulture "bad Int32" "en-US" <| fun () ->
+        x.testInvalidTryParse<int> "foo" ""
 
-    [<Test>] [<SetCulture("en-US")>]
-    member x.``good Double`` () = x.testCorrectTryParse<float> "3.14159" "" "IsEqualTo(3.14159)"
+    testWithCulture "good Double" "en-US" <| fun () ->
+        x.testCorrectTryParse<float> "3.14159" "" "IsEqualTo(3.14159)"
 
-    [<Test>] [<SetCulture("fr-FR")>]
-    member x.``good Double fr-FR`` () = x.testCorrectTryParse<float> "3,14159" "" "IsEqualTo(3.14159)"
+    testWithCulture "good Double fr-FR" "fr-FR" <| fun () ->
+        x.testCorrectTryParse<float> "3,14159" "" "IsEqualTo(3.14159)"
 
-    [<Test>]
-    member x.``good Double fr-FR with NumberStyles`` () =
+    test "good Double fr-FR with NumberStyles" {
         x.testCorrectTryParse<float>
             "3,14159"
             @", NumberStyles.Any, CultureInfo.GetCultureInfo(""fr-FR"")"
             "IsEqualTo(3.14159)"
+    }
 
-    [<Test>] [<SetCulture("en-US")>]
-    member x.``bad Double`` () = x.testInvalidTryParse<float> "foo" ""
+    testWithCulture "bad Double" "en-US" <| fun () ->
+        x.testInvalidTryParse<float> "foo" ""
 
-    [<Test>]
-    member x.``good Guid`` () =
+    test "good Guid" {
          x.testCorrectTryParse<Guid>
             "{0050AC52-49E9-11E5-95FD-03788BD11E5D}"
             ""
             @"IsEqualTo(new Guid(""{0050AC52-49E9-11E5-95FD-03788BD11E5D}""))"
+    }
 
-    [<Test>]
-    member x.``bad Guid`` () =  x.testInvalidTryParse<Guid> "foo" ""
+    test "bad Guid" {
+        x.testInvalidTryParse<Guid> "foo" ""
+    }
+]
 
-[<TestFixture>]
-type TryParse() =
-    inherit ParsingTests()
+let tests = [
+    testList "TryParse" (standardTests {
+        new IParseTests with
+            member __.testCorrectTryParse<'t> input additionalArgs additionalTest =
+                let idType = makeIdFromType<'t> id
+                let test = sprintf @"
+                    Id instance;
+                    var result = Id.TryParse(""%s""%s, out instance);
+                    Check.That(result).IsTrue();
+                    Check.That(instance.Value).%s;
+                    "
+                            input additionalArgs additionalTest
 
-    override x.testCorrectTryParse<'t> input additionalArgs additionalTest =
-        let idType = makeIdFromType<'t> id
-        let test = sprintf @"
-            Id instance;
-            var result = Id.TryParse(""%s""%s, out instance);
-            Check.That(result).IsTrue();
-            Check.That(instance.Value).%s;
-            "
-                    input additionalArgs additionalTest
+                runGeneratedTest idType test
 
-        runGeneratedTest idType test
+            member __.testInvalidTryParse<'t> input additionalArgs =
+                let idType = makeIdFromType<'t> id
+                let test = sprintf @"
+                    Id instance;
+                    var result = Id.TryParse(""%s""%s, out instance);
+                    Check.That(result).IsFalse();
+                    "
+                            input additionalArgs
 
-    override x.testInvalidTryParse<'t> input additionalArgs =
-        let idType = makeIdFromType<'t> id
-        let test = sprintf @"
-            Id instance;
-            var result = Id.TryParse(""%s""%s, out instance);
-            Check.That(result).IsFalse();
-            "
-                    input additionalArgs
+                runGeneratedTest idType test
+    })
 
-        runGeneratedTest idType test
-
-[<TestFixture>]
-type ``TryParse nullable``() =
-    inherit ParsingTests()
-
-    override x.testCorrectTryParse<'t> input additionalArgs additionalTest =
-        let idType = makeIdFromType<'t> id
+    testList "TryParse nullable" (standardTests {
+        new IParseTests with
+            member __.testCorrectTryParse<'t> input additionalArgs additionalTest =
+                let idType = makeIdFromType<'t> id
     
-        let test = sprintf @"
-            var result = Id.TryParse(""%s""%s);
-            Check.That(result).IsNotNull();
-            Check.That(result.Value.Value).%s;
-            "
-                    input additionalArgs additionalTest
+                let test = sprintf @"
+                    var result = Id.TryParse(""%s""%s);
+                    Check.That(result).IsNotNull();
+                    Check.That(result.Value.Value).%s;
+                    "
+                            input additionalArgs additionalTest
 
-        runGeneratedTest idType test
+                runGeneratedTest idType test
 
-    override x.testInvalidTryParse<'t> input additionalArgs =
-        let idType = makeIdFromType<'t> id
-        let test = sprintf @"
-            var result = Id.TryParse(""%s""%s);
-            Check.That(result).IsNull();
-            "
-                    input additionalArgs
+            member __.testInvalidTryParse<'t> input additionalArgs =
+                let idType = makeIdFromType<'t> id
+                let test = sprintf @"
+                    var result = Id.TryParse(""%s""%s);
+                    Check.That(result).IsNull();
+                    "
+                            input additionalArgs
 
-        runGeneratedTest idType test
+                runGeneratedTest idType test
+    })
 
-[<TestFixture>]
-type Parse() =
-    inherit ParsingTests()
-
-    override x.testCorrectTryParse<'t> input additionalArgs additionalTest =
-        let idType = makeIdFromType<'t> id
+    testList "Parse" (standardTests {
+        new IParseTests with
+            member __.testCorrectTryParse<'t> input additionalArgs additionalTest =
+                let idType = makeIdFromType<'t> id
     
-        let test = sprintf @"
-            Check.ThatCode(() => Id.Parse(""%s""%s)).DoesNotThrow();
-            var result = Id.Parse(""%s""%s);
-            Check.That(result.Value).%s;
-            "
-                    input additionalArgs input additionalArgs additionalTest
+                let test = sprintf @"
+                    Check.ThatCode(() => Id.Parse(""%s""%s)).DoesNotThrow();
+                    var result = Id.Parse(""%s""%s);
+                    Check.That(result.Value).%s;
+                    "
+                            input additionalArgs input additionalArgs additionalTest
 
-        runGeneratedTest idType test
+                runGeneratedTest idType test
 
-    override x.testInvalidTryParse<'t> input additionalArgs =
-        let idType = makeIdFromType<'t> id
-        let test = sprintf @"
-            Check.ThatCode(() => Id.Parse(""%s""%s)).Throws<FormatException>();
-            "
-                    input additionalArgs
+            member __.testInvalidTryParse<'t> input additionalArgs =
+                let idType = makeIdFromType<'t> id
+                let test = sprintf @"
+                    Check.ThatCode(() => Id.Parse(""%s""%s)).Throws<FormatException>();
+                    "
+                            input additionalArgs
 
-        runGeneratedTest idType test
+                runGeneratedTest idType test
+    })
+]
+
+[<Tests>]
+let test = testList "Parse" tests
