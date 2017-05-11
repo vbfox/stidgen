4155f8821034861039910a8756cb1e9f9ac81c2f
Julien Roncaglia <julien@roncaglia.fr>
2017-05-11 22:45:16 +0200

    Use computation expressions in TaskDefinitionHelper

--- build/TaskDefinitionHelper.fsx
+++ build/TaskDefinitionHelper.fsx
@@ -6,45 +6,86 @@ namespace BlackFox
 [<AutoOpen>]
 module TaskDefinitionHelper =
     open Fake
+    open System
     open System.Text.RegularExpressions
 
     type Dependency =
         | Direct of dependsOn : string
         | Soft of dependsOn : string
-        | Conditional of dependsOn : string * condition : bool
 
-    let private (|RegExp|_|) pattern input =
+    let inline private (|RegExp|_|) pattern input =
         let m = Regex.Match(input, pattern, RegexOptions.Compiled)
         if m.Success && m.Groups.Count > 0 then
             Some (m.Groups.[1].Value)
         else
             None
 
-    let private parseDependency str =
+    let inline private parseDependency str =
         match str with
         | RegExp @"^\?(.*)$" dep -> Soft dep
         | dep -> Direct dep
 
-    let mutable private tasks : (string * (Dependency list)) list = []
+    let inline private parseDependencies dependencies =
+        [
+            for dependency in dependencies do
+                yield parseDependency dependency
+        ]
 
-    /// Define a task with it's dependencies
-    let TaskEx name dependencies body =
-        Target name body
-        tasks <- (name, dependencies |> List.ofSeq) :: tasks
+    type TaskMetadata = {
+        name: string
+        dependencies: Dependency list
+    }
+
+    let mutable private tasks : TaskMetadata list = []
+
+    let inline private registerTask meta body =
+        Target meta.name body
+        tasks <- meta::tasks
+
+    type TaskBuilder(meta: TaskMetadata) =
+        member __.TryFinally(f, compensation) =
+            try
+                f()
+            finally
+                compensation()
+        member __.TryWith(f, catchHandler) =
+            try
+                f()
+            with e -> catchHandler e
+        member __.Using(disposable: #IDisposable, f) =
+            try
+                f disposable
+            finally
+                match disposable with
+                | null -> ()
+                | disp -> disp.Dispose()
+        member __.For(sequence, f) =
+            for i in sequence do f i
+        member __.Combine(f1, f2) = f2(); f1
+        member __.Zero() = ()
+        member __.Delay f = f
+        member __.Run f =
+            registerTask meta (fun () -> f())
 
     /// Define a task with it's dependencies
     let Task name dependencies body =
-        let dependencies = dependencies |> Seq.map parseDependency
-        TaskEx name dependencies body
+        registerTask { name = name; dependencies = parseDependencies dependencies } body
+
+    /// Define a task with it's dependencies
+    let inline task name dependencies =
+        TaskBuilder({ name = name; dependencies = parseDependencies dependencies })
+
+    /// Define a task without any body, only dependencies
+    let inline EmptyTask name dependencies =
+        registerTask { name = name; dependencies = parseDependencies dependencies } (fun () -> ())
 
     /// Send all the defined inter task dependencies to FAKE
     let ApplyTasksDependencies () =
-         for (targetName, dependencies) in tasks do
-            for dependency in dependencies do
+         for taskMetadata in tasks do
+            for dependency in taskMetadata.dependencies do
                 match dependency with
-                | Direct dep -> dep ==> targetName |> ignore
-                | Soft dep -> dep ?=> targetName |> ignore
-                | Conditional (dep, cond) -> dep =?> (targetName, cond) |> ignore
+                | Direct dep -> dep ==> taskMetadata.name |> ignore
+                | Soft dep -> dep ?=> taskMetadata.name |> ignore
 
          tasks <- []
 
